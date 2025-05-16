3bba4cc384e215334f1b4f2c9239933296ffa1cf
L. Bálint <lakatos.balint@techtra.hu>
2025-05-16 18:56:14 +0200

    made writing to sync (idk idc it works)

--- video_hanlding/write_video.py
+++ video_hanlding/write_video.py
@@ -1,5 +1,4 @@
 import os
-import threading
 import cv2
 from loguru import logger
 import numpy as np
@@ -8,38 +7,23 @@ from utils.file_management import full_path
 
 
 def write_video(frames: list[np.ndarray], video_path: str, fps: int = 60):
-    def _write_video(frames: list[np.ndarray], video_path: str, fps: int = 60):
-        """Write frames to a video file in a separate thread."""
+    """Write frames to a video file (synchronously)."""
+    video_path = full_path(video_path)
 
-        video_path = full_path(video_path)
-
-        try:
-            writer = cv2.VideoWriter(
-                video_path,
-                cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
-                fps,
-                (frames[0].shape[1], frames[0].shape[0])
-            )
-            for frame in frames:
-                writer.write(frame)
-            writer.release()
-
-            logger.debug(f"Saved video: {os.path.basename(video_path)}")
-
-            return True
-
-        except Exception as e:
-            print(e)
-            logger.error(f"{type(e).__name__} at Video Writing!")
-            raise
-            # return False
-
-    threading.Thread(
-        target=_write_video,
-        daemon=True,
-        args=(
-            frames,
+    try:
+        writer = cv2.VideoWriter(
             video_path,
-            fps
-        ),
-    ).start()
+            cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
+            fps,
+            (frames[0].shape[1], frames[0].shape[0])
+        )
+        for frame in frames:
+            writer.write(frame)
+        writer.release()
+
+        logger.debug(f"Saved video: {os.path.basename(video_path)}")
+        return True
+
+    except Exception as e:
+        logger.error(f"{type(e).__name__} at Video Writing: {e}")
+        raise
